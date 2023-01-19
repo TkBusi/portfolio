4fcf3d14d643314be54f730e54e4261dacdd9fc8
Tom SHEN <tomshen@Toms-MacBook-Air.local>
2023-01-19 16:01:53 -0500

    word animation

--- src/home.tsx
+++ src/home.tsx
@@ -16,18 +16,20 @@ function Home() {
     }, 1000);
     return () => clearInterval(interval);
   }, []);
-
+  /*
   let i0 = Math.floor(seconds % 15 / 5);
   let i1 = Math.floor(seconds % 15 / 3);
   let i2 = Math.floor(seconds % 15);
+  */
+ let i0 = 0, i1 = 0, i2 = 0;
 
   return (
     <div className="home">
       <div className="intro">
-        <p>Hi I am Tom.</p>
-        <p>I am a {position[i0]}.</p>
-        <p>I code {language[i1]}.</p>
-        <p>I shine at {skills[i2]}.</p>
+        <div><span>Hi I am Tom.</span></div>
+        <div><span>I am a {position[i0]}.</span></div>
+        <div><span>I code {language[i1]}.</span></div>
+        <div><span>I shine at {skills[i2]}.</span></div>
       </div>
     </div>
   );
