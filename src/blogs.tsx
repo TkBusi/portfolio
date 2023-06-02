66b561339284e3de56f7fb37ffac2f994c8217ed
Tom SHEN <tomshen@Toms-MacBook-Air.local>
2023-06-02 18:13:50 -0400

    update on blogs

--- src/blogs.tsx
+++ src/blogs.tsx
@@ -1,14 +1,59 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import './blogs.css';
+// fetch blogs from files later
+let blogs = [
+  {
+    id: "1",
+    title:"title1",
+    date:"2023-03-04",
+    content:"hahahhahahhahhahahajjajdfasfjlewfjo"
+  },
+  {
+    id: "2",
+    title:"title2",
+    date:"2023-03-04",
+    content:"asudiofhewuadhhdsifiadsfsdhfdoashdfilo"
+  },
+  {
+    id: "3",
+    title:"title3",
+    date:"2023-04-05",
+    content:"hajsd;fhdlasfuhlsadhflsahdfhlasdf"
+  },
+  {
+    id: "4",
+    title:"title4",
+    date:"2023-03-06",
+    content:"dsilhfleawyhfhdlsaflhsdf"
+  },
+]
 
 function Blogs() {
+  const [monthIndex, setMonthIndex] = useState()
   useEffect(() => {
     document.title = "Tom Shen | Blogs";
   }, []);
   return (
     <div className="blogs">
-        blogs
+        <div className='catogory-by-month'>
+          {
+            // group by month
+            blogs.map(i => i.date.split("-")[0] + "-" + i.date.split("-")[1])
+            .filter((value,index,array) => array.indexOf(value) === index)
+            .map((im) =>
+              <div>
+                {im}
+              </div>
+            )
+          }
+        </div>
+        <hr/>
+        <div className='blogs'>
+          {
+
+          }
+        </div>
     </div>
   );
 }
